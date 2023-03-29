85bb8ebbbc66c66419381a63708e951e17244fa6
Den <potarykindenis@gmail.com>
2023-03-29 23:00:03 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './s1-main/App'
+
+
+import {Provider} from 'react-redux'
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  
+       
+        
+            <App/>,
+        
+    
+)
